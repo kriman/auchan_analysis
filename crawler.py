f860fd0d68aacedc0788e4917286e8c2d0198635
Kófiás Kristóf <kristof.kofias@protonmail.com>
2023-01-15 21:26:07 +0100

    major refactor using tree api - verification needed

--- crawler.py
+++ crawler.py
@@ -1,17 +1,18 @@
 import io
 import json
-import os
-import scrapy
-from scrapy.exceptions import DropItem
 import tarfile
+import time
 
-PREVIOUS_SCRAPE = "databases/archives/2022_12_27_rawdata.tar.xz"
-NEW_SCRAPE = "databases/archives/2023_01_08_rawdata_diff.tar"
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+PREVIOUS_SCRAPE = "databases/archives/2023_01_08_rawdata.tar.xz"
+NEW_SCRAPE = "databases/archives/2023_01_15_rawdata_diff.tar"
 
 
 class AuchanSpider(scrapy.Spider):
     name = 'auchan_spider'
-    start_urls = ['https://online.auchan.hu/api/v2/products?page=1&itemsPerPage=12&listId=10177&hl=hu']
+    start_urls = ['https://online.auchan.hu/']
 
     custom_settings = {
         'ITEM_PIPELINES': {
@@ -28,15 +29,6 @@ class AuchanSpider(scrapy.Spider):
         }
     }
 
-    """
-    'FEEDS': {
-            'products2.csv': {
-                'format': 'csv',
-                'overwrite': True
-            }
-        },
-    """
-
     PRODUCTS_PER_PAGE = 500
     BASE_URL = 'https://online.auchan.hu/api/v2'
 
@@ -47,134 +39,153 @@ class AuchanSpider(scrapy.Spider):
         'description': 'description',
         'allergens': 'allergenInfo'
     }
+
     PRODUCT_FIELDS = {
         'stock_infos': None
     }
 
-    # authorization = open('authorization.txt', 'r').read()
-    authorization = str()
+    authorization = ""
 
     tar = tarfile.open(PREVIOUS_SCRAPE, "r:xz")
 
+    tar_names = []
+
+    start_time = -1
+
     def start_requests(self):
         self.tar_names = self.tar.getnames()
+        self.start_time = time.time()
         yield scrapy.Request(
             url='https://online.auchan.hu/',
         )
 
     def end_requests(self):
         self.tar.close()
+        print(f"Time elapsed: {time.time() - self.start_time}")
 
-    def _request(self, url):
+    def _request(self, url, callback=None, **kwargs):
+        if callback is None:
+            callback = self.parse
         request = scrapy.Request(
             url=url,
-            callback=self.parse)
+            callback=callback)
         for key, value in self.custom_settings['DEFAULT_REQUEST_HEADERS'].items():
             request.headers[key] = value
-        request.headers["Authorization"] = (self.authorization)
+        request.headers["Authorization"] = self.authorization
         return request
 
-    def parse(self, response):
-
-        def _get_details(details, itemId, variantId, detail_field="detail/"):
-            print(f'Parsing details for {itemId}/{variantId}')
-            print(details)
-            for field in details:
-                # if not in tarfile
-                print(f'Parsing {field}')
-                if f'rawdata/{itemId}/{variantId}/{field}.json' not in self.tar_names:
-                    print("not in tarfile")
-                    yield self._request(
-                        url=f'{self.BASE_URL}/products/{itemId}/variants/{variantId}/{detail_field}{field}?hl=hu')
-                else:
-                    # yield back from the tarfile
-                    print(f'yielding back from tarfile: {f"rawdata/{itemId}/{variantId}/{field}.json"}')
-                    yield {
-                        field: {
-                            'itemId': itemId,
-                            'variantId': variantId,
-                            'data': json.loads(
-                                self.tar.extractfile(f'rawdata/{itemId}/{variantId}/{field}.json').read())
-                        }
-                    }
-            return True
-
-        # parse json response
-        if response.url == 'https://online.auchan.hu/':
-            cookies = response.headers.getlist('Set-Cookie')
-            print(cookies)
-            for cookie in cookies:
-                if cookie.startswith(b'access_token'):
-                    access_token = cookie.split(b'=')[1].split(b';')[0].decode('utf-8')
-                    self.custom_settings['DEFAULT_REQUEST_HEADERS']['Authorization'] = f'Bearer {access_token}'
-                    self.authorization = f'Bearer {access_token}'
-                    print(self.authorization)
-                    break
-
-            for category in range(15000, 5500, -1):
-                yield self._request(
-                    url=f'{self.BASE_URL}/products?page=1&itemsPerPage={self.PRODUCTS_PER_PAGE}&categoryId={category}&hl=hu')
+    def parse_tree(self, node, children, leafs_id):
+        if node["childCount"] == 0:
+            if node["level"] == 3:
+                if "icon" in node:
+                    del node["icon"]
+                leafs_id.append(node["id"])
 
         else:
-            data = json.loads(response.text)
-            if 'results' in data:
-                print(f'Parsing category: {data["results"][0]["categoryId"]}')
-                for product in data['results']:
-                    itemId = product['id']
-                    variantId = product['selectedVariant']['id']
-                    # if not os.path.exists(f'rawdata/{itemId}/{variantId}/product.json'):
-                    yield {
-                        'product': {
-                            'itemId': itemId,
-                            'variantId': variantId,
-                            'data': product
-                        }
-                    }
+            for child in node["children"]:
+                self.parse_tree(child, children, leafs_id)
 
-                    # get details
-                    details = product['selectedVariant']['details']
-                    for field in details:
-                        # if not in tarfile
-                        if f'rawdata/{itemId}/{variantId}/{field}.json' not in self.tar_names:
-                            yield self._request(
-                                url=f'{self.BASE_URL}/products/{itemId}/variants/{variantId}/details/{field}?hl=hu')
+        children.append(node)
 
-                    for field in self.PRODUCT_FIELDS:
-                        # if not in tarfile
-                        if f'rawdata/{itemId}/{variantId}/{field}.json' not in self.tar_names:
-                            yield self._request(
-                                url=f'{self.BASE_URL}/products/{itemId}/variants/{variantId}/{field}?hl=hu')
+    def parse_categories(self, response):
+        print("parse_categories")
+        tree = json.loads(response.text)
+        children = []
+        leafs_id = []
+        self.parse_tree(tree, children, leafs_id)
+        for leaf_id in leafs_id:
+            yield self._request(
+                url=f'{self.BASE_URL}/products?categoryId={leaf_id}&page=1&itemsPerPage={self.PRODUCTS_PER_PAGE}&hl=hu',
+                callback=self.parse_products
+            )
+        # tree.json is not saved because it can be generated from product files
 
-                # go to next product page
-                if response.json()['currentPage'] < response.json()['pageCount']:
-                    category = response.url.split('categoryId=')[1].split('&')[0]
-                    next_page = response.json()['currentPage'] + 1
-                    next_url = f'{self.BASE_URL}/products?page={next_page}&itemsPerPage={self.PRODUCTS_PER_PAGE}&categoryId={category}&hl=hu'
-                    yield self._request(url=next_url)
-
-            elif 'sectionType' in data:
-                itemId = response.url.split('/')[-5]
-                variantId = response.url.split('/')[-3]
-
-                detail = response.url.split('/')[-1].split('?')[0]
-                field = self.DETAIL_FIELDS[detail]
-                yield {
-                    detail: {
-                        'itemId': itemId,
-                        'variantId': variantId,
-                        'data': data[field]
-                    }
+    def parse_products(self, response):
+        data = json.loads(response.text)
+        print(f'Parsing category: {response.url.split("categoryId=")[1].split("&")[0]}')
+        for product in data['results']:
+            itemId = product['id']
+            variantId = product['selectedVariant']['id']
+            yield {
+                'product': {
+                    'itemId': itemId,
+                    'variantId': variantId,
+                    'data': product
                 }
-            elif type(data) == list:
-                itemId = response.url.split('/')[-4]
-                variantId = response.url.split('/')[-2]
-                yield {
-                    'stock_infos': {
-                        'itemId': itemId,
-                        'variantId': variantId,
-                        'data': data
-                    }
-                }
+            }
+
+            yield from self.get_details(product, itemId, variantId)
+            yield from self.get_product_fields(itemId, variantId)
+
+        yield from self.get_next_page(response)
+
+    def get_next_page(self, response):
+        if response.json()['currentPage'] < response.json()['pageCount']:
+            category = response.url.split('categoryId=')[1].split('&')[0]
+            next_page = response.json()['currentPage'] + 1
+            next_url = f'{self.BASE_URL}/products?page={next_page}&itemsPerPage={self.PRODUCTS_PER_PAGE}&categoryId={category}&hl=hu'
+            yield self._request(url=next_url, callback=self.parse_products)
+
+    def get_product_fields(self, itemId, variantId):
+        for field in self.PRODUCT_FIELDS:
+            # if not in tarfile
+            if f'rawdata/{itemId}/{variantId}/{field}.json' not in self.tar_names:
+                yield self._request(
+                    url=f'{self.BASE_URL}/products/{itemId}/variants/{variantId}/{field}?hl=hu',
+                    callback=self.parse_product_fields)
+
+    def get_details(self, product, itemId, variantId):
+        details = product['selectedVariant']['details']
+        for field in details:
+            if f'rawdata/{itemId}/{variantId}/{field}.json' not in self.tar_names:
+                yield self._request(
+                    url=f'{self.BASE_URL}/products/{itemId}/variants/{variantId}/details/{field}?hl=hu',
+                    callback=self.parse_details)
+
+    def parse_details(self, response):
+        data = json.loads(response.text)
+
+        itemId = response.url.split('/')[-5]
+        variantId = response.url.split('/')[-3]
+
+        detail = response.url.split('/')[-1].split('?')[0]
+        field = self.DETAIL_FIELDS[detail]
+        yield {
+            detail: {
+                'itemId': itemId,
+                'variantId': variantId,
+                'data': data[field]
+            }
+        }
+
+    @staticmethod
+    def parse_product_fields(response):
+        data = json.loads(response.text)
+
+        itemId = response.url.split('/')[-4]
+        variantId = response.url.split('/')[-2]
+        yield {
+            'stock_infos': {
+                'itemId': itemId,
+                'variantId': variantId,
+                'data': data
+            }
+        }
+
+    def parse(self, response, **kwargs):
+        # set authorization
+        cookies = response.headers.getlist('Set-Cookie')
+        print(cookies)
+        for cookie in cookies:
+            if cookie.startswith(b'access_token'):
+                access_token = cookie.split(b'=')[1].split(b';')[0].decode('utf-8')
+                self.custom_settings['DEFAULT_REQUEST_HEADERS']['Authorization'] = f'Bearer {access_token}'
+                self.authorization = f'Bearer {access_token}'
+                print(self.authorization)
+                break
+
+        # get categories list by root category
+        yield self._request(url=f'{self.BASE_URL}/tree/0?hl=hu', callback=self.parse_categories)
 
 
 class RawDataPipeline(object):
@@ -200,26 +211,6 @@ class RawDataPipeline(object):
     def close_spider(self, spider):
         print("Closing spider")
         self.tar.close()
-
-
-class SaveBaseInfo(object):
-    def process_item(self, item, spider):
-        """text processing"""
-        if 'product' in item:
-            item = item['product']
-            return {
-                'id': item['id'],
-                'defaultVariant': item['defaultVariant']['id'],
-                'productId': item['defaultVariant']['productId'],
-                'price': item['defaultVariant']['price']['gross'],
-                'name': item['defaultVariant']['name'],
-                'categoryId': item['categoryId'],
-            }
-        else:
-            raise DropItem('not product')
-
-
-from scrapy.crawler import CrawlerProcess
 
 process = CrawlerProcess()
 res = process.crawl(AuchanSpider)
